4013c4e98e0f88001693c8521329ca0b56890da5
narasimhareddyprostack <narasimhareddyk18@gmail.com>
2022-12-26 11:20:09 +0530

    okie

---
+++ Part -1/VarArgs/Demo.java
@@ -0,0 +1,5 @@
+class Demo3{
+    public static void main(String[] raj){
+        System.out.println("......"+raj);
+    }
+}
---
+++ Part -1/cmdargs/Test.java
@@ -0,0 +1,9 @@
+class Test{
+    public static void main(String[] args){
+        int a = Integer.parseInt(args[0]);
+        double b = Double.parseDouble(args[1]);
+        //System.out.println(args[0] + args[1]); //1020
+        System.out.println(a + b);
+        
+    }
+}
