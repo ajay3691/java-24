3e731115e07a313465edea5a124ed1d63d70d73f
narasimhareddyprostack <narasimhareddyk18@gmail.com>
2023-01-10 09:55:51 +0530

    okie

---
+++ ExceptionHandling/Demo.java
@@ -0,0 +1,16 @@
+public class Demo{
+    public static void main(String[] args){
+        try{
+        System.out.println(10/0);
+        }
+        catch(Exception e){
+            //System.out.println(e.toString());
+            //e.printStackTrace();
+            System.out.println(e.getMessage());
+        }
+        finally{
+            System.out.println("Finally block always excecutes");
+        }
+        
+    }
+}
---
+++ ExceptionHandling/TestRead.java
@@ -0,0 +1,8 @@
+import java.io.*;
+
+public class TestRead{
+    public static void main(String[] args){
+        PrintWriter pw=new PrintWriter("abc.txt");
+        System.out.println(pw);
+    }
+}
